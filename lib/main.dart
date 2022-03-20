c81d90ef50880c8ba0fd64481761035df36138fd
Anirudh Jwala <anirudhsai2@gmail.com>
2022-03-20 09:26:19 +0530

    Splash screen and base setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+// Splash screen
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
@@ -12,9 +16,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Groww',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFF0abb92),
+        fontFamily: 'Roboto',
+      ),
+      darkTheme: ThemeData.dark().copyWith(
+        primaryColor: const Color(0xFF0abb92),
       ),
       home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -27,6 +36,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    FlutterNativeSplash.remove();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(

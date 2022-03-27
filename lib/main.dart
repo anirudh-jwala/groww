ebf0f0c0b030dd8f76eaa7de7c656812a7417ec8
Anirudh Jwala <anirudhsai2@gmail.com>
2022-03-27 17:56:59 +0530

    Login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
-// Splash screen
+
+// Widgets
+import 'package:groww/widgets/auth_loader.dart';
+
+// Packages
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
@@ -22,20 +27,20 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData.dark().copyWith(
         primaryColor: const Color(0xFF0abb92),
       ),
-      home: const MyHomePage(),
+      home: const AuthenticationPage(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class AuthenticationPage extends StatefulWidget {
+  const AuthenticationPage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<AuthenticationPage> createState() => _AuthenticationPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _AuthenticationPageState extends State<AuthenticationPage> {
   @override
   void initState() {
     super.initState();
@@ -45,14 +50,111 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const SizedBox(height: 40.0),
+                SvgPicture.asset(
+                  "assets/images/groww-logo-svg.svg",
+                  semanticsLabel: 'Groww',
+                  width: 130,
+                  height: 30,
+                ),
+                const SizedBox(height: 40.0),
+                const Text(
+                  "Simple",
+                  style: TextStyle(
+                    fontSize: 56.0,
+                    color: Color(0xFF44475B),
+                    fontWeight: FontWeight.w100,
+                  ),
+                ),
+                const Text(
+                  "investing.",
+                  style: TextStyle(
+                    fontSize: 56.0,
+                    color: Color(0xFF44475B),
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                const AuthLoader(),
+                const SizedBox(height: 50.0),
+                Align(
+                  alignment: Alignment.center,
+                  child: RichText(
+                    text: const TextSpan(
+                      text: 'By proceeding, I agree to the ',
+                      style: TextStyle(
+                        color: Color(0xFF7C7E8C),
+                      ),
+                      children: [
+                        TextSpan(
+                          text: 'T&C',
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontWeight: FontWeight.w500,
+                            decorationColor: Color(0xFF44475B),
+                          ),
+                        ),
+                        TextSpan(text: ' and '),
+                        TextSpan(
+                          text: 'Privacy Policy',
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontWeight: FontWeight.w500,
+                            decorationColor: Color(0xFF44475B),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                Align(
+                  alignment: Alignment.center,
+                  child: ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: SvgPicture.asset(
+                      "assets/images/google-color.svg",
+                      width: 25.0,
+                      height: 25.0,
+                    ),
+                    label: const Text(
+                      "Continue with Google",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 80.0,
+                        vertical: 14.0,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                const Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "Continue with Other Email",
+                    style: TextStyle(
+                      color: Color(0xFF00D09C),
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
---
+++ lib/widgets/auth_loader.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+// Animated text
+import 'package:animated_text_kit/animated_text_kit.dart';
+
+class AuthLoader extends StatefulWidget {
+  const AuthLoader({Key? key}) : super(key: key);
+
+  @override
+  State<AuthLoader> createState() => _AuthLoaderState();
+}
+
+class _AuthLoaderState extends State<AuthLoader>
+    with SingleTickerProviderStateMixin {
+  Animation? sizeAnimation;
+  AnimationController? controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 2500),
+    )..repeat();
+
+    sizeAnimation = Tween(begin: 0.0, end: 45.0).animate(
+      CurvedAnimation(
+        parent: controller!,
+        curve: Curves.easeInOut,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller!.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const investingOptionStyle = TextStyle(
+      fontWeight: FontWeight.w500,
+      fontSize: 18.0,
+      color: Color(0xFF7A7C8A),
+    );
+    const investingOptionDuration = Duration(milliseconds: 2500);
+
+    return AnimatedBuilder(
+      animation: controller!,
+      builder: (context, child) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                color: Color(0xFF00D09C),
+              ),
+              height: 15.0,
+              width: sizeAnimation!.value,
+            ),
+            const SizedBox(height: 10.0),
+            SizedBox(
+              width: 180.0,
+              height: 40.0,
+              child: AnimatedTextKit(
+                repeatForever: true,
+                animatedTexts: [
+                  FadeAnimatedText(
+                    'Stocks',
+                    duration: investingOptionDuration,
+                    textStyle: investingOptionStyle,
+                  ),
+                  FadeAnimatedText(
+                    'ETFs',
+                    duration: investingOptionDuration,
+                    textStyle: investingOptionStyle,
+                  ),
+                  FadeAnimatedText(
+                    'Direct Mutual Funds',
+                    duration: investingOptionDuration,
+                    textStyle: investingOptionStyle,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
